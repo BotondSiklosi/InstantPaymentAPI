bc421908997408ea0b459ee0cdd759a0d30f6a8b
Siki <kissiki99@gmail.com>
2025-03-10 19:25:10 +0100

    Created Instant Payment API repository

---
+++ src/main/java/hu/java/instantpaymentapi/InstantPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package hu.java.instantpaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantPaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantPaymentApiApplication.class, args);
+    }
+
+}
